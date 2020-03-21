a6cc3bc44fff9ffa8bd407c686626a4a6d1300e4
rahulmoorthy19 <rahulmoorthy9.6@gmail.com>
2020-03-21 11:10:35 +0530

    Added skeleton structure for flask api

--- flask-api/api.py
+++ flask-api/api.py
@@ -1,8 +1,18 @@
-import time
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+@app.route('/encode',methods=['POST'])
+def encode_text():
+    print("This is encoding the python structures function")
+
+
+@app.route('/structures',methods=['POST'])
+def get_structures():
+    print("This is the fuction which produces python structures")
+
+@app.route('/decode',methods=['POST'])
+def decode_text():
+    print("This is the function which decodes the python structures.")
+
+app.run(host='0.0.0.0', port=8658, debug=True)
