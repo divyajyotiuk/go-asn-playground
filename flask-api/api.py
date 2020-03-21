6eff929acea682e8097a340aeaaabdb3f98b7068
rahulmoorthy19 <rahulmoorthy9.6@gmail.com>
2020-03-21 13:30:04 +0530

    temperory structures code added

--- flask-api/api.py
+++ flask-api/api.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+import os
 app = Flask(__name__)
 
 @app.route('/encode',methods=['POST'])
@@ -9,7 +9,20 @@ def encode_text():
 
 @app.route('/structures',methods=['POST'])
 def get_structures():
+    send_dict={}
     print("This is the fuction which produces python structures")
+    text=eval(request.json["asn_text"])
+    file = open("test.asn1", "w")
+    for i in text:
+        f.write(i)
+        f.write("\n")
+    file.close()
+    os.system("python pycrate_asn1compile.py -i test.asn1")
+    file_new=f = open("output.py", "r")
+    output=file_new.read()
+    sending_list=output.split("\n")
+    send_dict["output"]=sending_list
+    return jsonify(send_dict)
 
 @app.route('/decode',methods=['POST'])
 def decode_text():
